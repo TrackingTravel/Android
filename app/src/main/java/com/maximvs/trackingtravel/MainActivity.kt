1dc557f4a0620d5d549a433cc8ca08227c706d83
NadezhdaSakal <79761587+NadezhdaSakal@users.noreply.github.com>
2022-10-31 15:09:09 +0300

    add fragments

--- app/src/main/java/com/maximvs/trackingtravel/MainActivity.kt
+++ app/src/main/java/com/maximvs/trackingtravel/MainActivity.kt
@@ -1,14 +1,20 @@
-package com.maximvs.trackingtravel
-
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.maximvs.trackingtravel.ActivityRequest
+import com.maximvs.trackingtravel.FirstFragment
+import com.maximvs.trackingtravel.R
+import com.maximvs.trackingtravel.RequestFragment
 import com.maximvs.trackingtravel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //initNavigation()
+
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -16,5 +22,26 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this@MainActivity, ActivityRequest::class.java)
             startActivity(intent)
         })
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fragment_placeholder, FirstFragment())
+            .addToBackStack(null)
+            .commit()
+
+
     }
+
+    fun launchRequestFragment() {
+        val bundle = Bundle()
+        bundle.putParcelable()
+        val fragment = RequestFragment()
+        fragment.arguments = bundle
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_placeholder, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
+
 }
---
+++ app/src/main/java/com/maximvs/trackingtravel/RequestFragment.kt
@@ -0,0 +1,19 @@
+package com.maximvs.trackingtravel
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class RequestFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_request, container, false)
+    }
+
+
+}
