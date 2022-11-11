d9cf7ca8073f38d4e495f63d7d3db15e21f254ba
NadezhdaSakal <79761587+NadezhdaSakal@users.noreply.github.com>
2022-11-11 19:30:36 +0300

    add strings

--- app/src/main/java/com/maximvs/trackingtravel/view/fragments/StartFragment.kt
+++ app/src/main/java/com/maximvs/trackingtravel/view/fragments/StartFragment.kt
@@ -22,8 +22,8 @@ class StartFragment : Fragment () {
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+   /* override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-    }
+    } */
 }
